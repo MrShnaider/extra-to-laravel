4162700aff1a6458a82f09879e1f5d4d8539e58c
MrShnaider <alex-fktrc@yandex.ru>
2022-01-04 03:32:35 +0300

    feat(mix): Add mix

---
+++ webpack.mix.js
@@ -0,0 +1,71 @@
+const mix = require('laravel-mix');
+const fs = require("fs");
+
+
+/**
+ * Returns an array of paths to files in the 'dir' directory with the '_extension' extension
+ * @param {[]} [files_] You can pass an existing array, new entries will be appended to the end
+ * @returns {string[]}
+ */
+function getFiles(dir, _extension, files_ = []){
+	files_ = files_ || [];
+	let files = fs.readdirSync(dir);
+	for (let i in files){
+		let name = dir + '/' + files[i];
+		if (fs.statSync(name).isDirectory()){
+			getFiles(name, _extension, files_);
+		} else {
+			if(name.endsWith(_extension)){
+				files_.push(name);
+			}
+		}
+	}
+	return files_;
+};
+
+let Pages = {
+	handleTypeScript = () => {
+		for(item of getFiles("resources/views/pages", ".ts")) {
+			let itemName = item.split("/");
+			mix.ts(item, "public/scripts/" + itemName[itemName.length - 1].split(".")[0]+".js");
+		}
+	},
+	handleStyles = () => {
+		for(item of getFiles("resources/views/pages", ".scss")) {
+			let itemName = item.split("/");
+			mix.sass(item, "public/styles/");
+		}
+	},
+	handleAll = () => {
+		this.handleStyles();
+		this.handleTypeScript();
+	}
+}
+
+let Templates = {
+	handleTypeScript = () => {
+		for(item of getFiles("resources/views/templates", ".ts")) {
+			let itemName = item.split("/");
+			mix.ts(item, "public/scripts/" + itemName[itemName.length - 1].split(".")[0]+".js");
+		}
+	},
+	handleStyles = () => {
+		for(item of getFiles("resources/views/templates", ".scss")) {
+			let itemName = item.split("/");
+			mix.sass(item, "public/styles/");
+		}
+	},
+	handleAll = () => {
+		this.handleStyles();
+		this.handleTypeScript();
+	}
+}
+
+Pages.handleAll();
+Templates.handleAll();
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+mix.disableNotifications();
